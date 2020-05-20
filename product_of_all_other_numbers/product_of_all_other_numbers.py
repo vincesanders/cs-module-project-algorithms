34e6f932cb620fdf2545405daf547e5d94ee1f08
Vincent Sanders <vincesanders@hotmail.com>
2020-05-20 14:53:52 -0500

    Finished a close to optimized solution for product of all other numbers challenge.

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,38 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
+def product_of_all_other_numbers(arr): # Time: O(2n) Space: O(1)
     # Your code here
-
-    pass
+    # find the product O(n)
+    num_zeros = 0
+    product = 1
+    nonzero_product = 1
+    for i in arr:
+        product *= i
+        if i is 0:
+            num_zeros += 1
+        else:
+            nonzero_product *= i
+    if num_zeros > 1:
+        for i in range(len(arr)):
+            arr[i] = 0
+        return arr
+    # multiply each number by the product O(n)
+    for i in range(len(arr)):
+        if product is 0 and arr[i] is not 0:
+            arr[i] = 0
+        elif product is 0 and arr[i] is 0:
+            arr[i] = nonzero_product
+        else:
+            local_product = product // arr[i]
+            arr[i] = local_product
+    return arr
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 7, 3, 4]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
