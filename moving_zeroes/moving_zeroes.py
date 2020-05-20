2de3fcd7a81a5b32402ba05cadd4ec88c3e97168
Vincent Sanders <vincesanders@hotmail.com>
2020-05-20 13:47:59 -0500

    Finished first pass solution of moving zeros.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,18 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
+def moving_zeroes(arr): # Time: O(n^2) Space: O(1)
     # Your code here
-
-    pass
+    last_zero = 0
+    for i in range(len(arr)):
+        if arr[i] is 0:
+            # add 0 to beginning
+            arr[0] = 0
+            # shift elements right
+            for j in reversed(range(last_zero, i + 1)):
+                arr[j] = arr[j - 1]
+            last_zero += 1
+    return arr
 
 
 if __name__ == '__main__':
