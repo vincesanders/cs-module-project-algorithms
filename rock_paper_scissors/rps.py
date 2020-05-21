e6ae6673a8ba46964d4f517a707e5338d3cbce8e
Vincent Sanders <vincesanders@hotmail.com>
2020-05-21 09:55:13 -0500

    First draft non-working solution of rps.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,9 +4,33 @@ import sys
 
 def rock_paper_scissors(n):
   # Your code here
+  moves = {
+    0: 'rock',
+    1: 'paper',
+    2: 'scissors'
+  }
+  permutations = []
+  if n is 0:
+    permutations.append([])
+  for _ in range(n):
+    for j in range(3):
+      permutations.append(moves[j]) # Get list of all possible permutations
+  # flip solution 90 degrees
+  solution = []
+  index = 0
+  while index < len(permutations):
+    round = []
+    for i in range(n):
+      round.append(permutations[index])
+      index += 1
+    solution.append(round)
 
-  pass
 
+  return solution
+  # go through each round
+
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
